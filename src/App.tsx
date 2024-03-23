99bb2a6d6c5c5441bae65707c104b9bcc29a001a
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-03-23 13:55:28 +0200

    add user form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import AdviceGenerator from "./components/AdviceGenerator";
+import UserForm from "./components/UserForm";
 
 function App() {
   return (
     <>
       <AdviceGenerator />
+      <UserForm userFormType={"Register"} />
+      <UserForm userFormType={"Login"} />
     </>
   );
 }
