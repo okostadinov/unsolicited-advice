1baea33c7a144561ce6f25f573c9015828923e04
Oleg Kostadinov <o.o.kostadinov@gmail.com>
2024-03-27 13:36:18 +0200

    style alert, add alert variants

--- src/components/alert-dialog.tsx
+++ src/components/alert-dialog.tsx
@@ -2,11 +2,28 @@ import { useAlert } from "../utils/alert";
 
 export enum AlertType {
   Success = "success",
+  Warning = "warning",
+  Danger = "danger",
 }
 
 export type Alert = {
   message: string;
   type: AlertType;
+};
+
+const alertTypeStyles = {
+  success: {
+    dialog: "border-green-900 bg-green-200",
+    message: "text-green-900",
+  },
+  warning: {
+    dialog: "border-yellow-900 bg-yellow-200",
+    message: "text-yellow-900",
+  },
+  danger: {
+    dialog: "border-red-900 bg-red-200",
+    message: "text-red-900",
+  },
 };
 
 const Alert = () => {
@@ -18,9 +35,23 @@ const Alert = () => {
   };
   return (
     alert.message && (
-      <dialog open className={alert.type} id="alert-dialog">
-        <p>{alert.message}</p>
-        <button onClick={closeDialog}>Close</button>
+      <dialog
+        open
+        className={`top-20 py-4 px-6 border rounded ${
+          alertTypeStyles[alert.type].dialog
+        }`}
+        id="alert-dialog"
+      >
+        <p
+          className={`inline-block font-serif font-medium text-xl mr-4 ${
+            alertTypeStyles[alert.type].message
+          }`}
+        >
+          {alert.message}
+        </p>
+        <button className="text-xl text-stone-600" onClick={closeDialog}>
+          &#x2715;
+        </button>
       </dialog>
     )
   );
